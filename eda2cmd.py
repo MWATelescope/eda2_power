f7a03308d7ed0d053da31ad661644db7e4953e60
Andrew Williams <Andrew.Williams@curtin.edu.au>
2019-09-27 16:08:52 +0800

    Allow use of tile names in fndh commands as well as A1-D8 output names

--- eda2cmd.py
+++ eda2cmd.py
@@ -196,7 +196,7 @@ if __name__ == '__main__':
                     ostrings.append('<%s: %3s: %6.3f V, %6.3f mA>' % (oname, pstate, v, i))
                 else:
                     ostrings.append('%s: Unknown.' % oname)
-            print('Tile %d: %s, %s' % (tid, ostrings[0], ostrings[1]))
+            print('Tile %2d: %s, %s' % (tid, ostrings[0], ostrings[1]))
     elif action == 'version':
         result = proxy.version()
         print('Software Version running on %s is: %s' % (cname, result))
